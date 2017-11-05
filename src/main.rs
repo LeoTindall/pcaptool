4af7d4db046ccade2a975a94edac4587ad2ac3b8
LeoTindall <lfstindall@gmail.com>
2017-11-05 14:40:51 -0600

    Use Unicode table characters for table output.
    Thanks to @noiob@awoo.space for the suggestion.

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,18 @@ use prettytable::{Table, format};
 fn main() {
     let devices = Device::list().expect("Could not list devices");
 
+    let format = format::FormatBuilder::new()
+        .column_separator('│')
+        .borders('│')
+        .separators(
+            &[format::LinePosition::Top, format::LinePosition::Title, format::LinePosition::Bottom],
+            format::LineSeparator::new('─', '┼', '├', '┤')
+        )
+        .padding(1, 1)
+        .build();
+
     let mut table = Table::new();
-    table.set_format(*format::consts::FORMAT_NO_LINESEP_WITH_TITLE); 
+    table.set_format(format); 
     table.set_titles(row![b->"Device Name", b->"Description"]);
 
     for device in devices {
